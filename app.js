17d152dd4e3b7ed235e9ed0c988a3a9477253ed8
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-12-23 16:35:51 +0300

    Тест, что вообще до докера доходят запросы

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var router = require('./router');
 var http = require('http');
 
 http.createServer(function(request, response) {
-  router.home(request, response);
-  router.user(request, response);
+  response.write('hello');
+  response.end();
+  // router.home(request, response);
+  // router.user(request, response);
 }).listen(8080);
 
 console.log('Server running!🍿');
